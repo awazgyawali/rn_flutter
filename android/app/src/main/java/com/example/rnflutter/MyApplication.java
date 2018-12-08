0a813a7ca51fa01d4bb148d5450e57d4843e7f06
Aawaz <awazgyawali@gmail.com>
2018-12-08 20:23:55 +0545

    Worked on debug build for RN

--- android/app/src/main/java/com/example/rnflutter/MyApplication.java
+++ android/app/src/main/java/com/example/rnflutter/MyApplication.java
@@ -1,10 +1,42 @@
 package com.example.rnflutter;
 
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
+
+import java.util.Arrays;
+import java.util.List;
 
 import io.flutter.app.FlutterApplication;
 
-public class MyApplication extends FlutterApplication {
+public class MyApplication extends FlutterApplication implements ReactApplication {
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage()
+            );
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
 
     @Override
     public void onCreate() {
--- android/app/src/main/java/com/example/rnflutter/MyReactNativeActivity.java
+++ android/app/src/main/java/com/example/rnflutter/MyReactNativeActivity.java
@@ -1,11 +1,6 @@
 package com.example.rnflutter;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MyReactNativeActivity extends ReactActivity {
 
@@ -15,22 +10,4 @@ public class MyReactNativeActivity extends ReactActivity {
     }
 
 
-    @Override
-    public boolean getUseDeveloperSupport() {
-        return BuildConfig.DEBUG;
-    }
-
-    @Override
-    protected List<ReactPackage> getPackages() {
-        return Arrays.<ReactPackage>asList(
-                new MainReactPackage()
-        );
-    }
-
-
-
-    @Override
-    protected String getJSMainModuleName() {
-        return "index";
-    }
 }
